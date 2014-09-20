46e96f4886fef907f0e88d0afb21d4208526c3d0
arosca <contact@arosca.ro>
2014-09-20 12:05:55 +0300

    contact form

--- app/scripts/components/pages/contact/index.js
+++ app/scripts/components/pages/contact/index.js
@@ -10,9 +10,21 @@ var ContactPage = React.createClass({
         return (
             <div>
                 <h1>Contact</h1>
+                <form onSubmit={this._handleSubmit}>
+                	<textarea id="message" placeholder="Message" ref="message"></textarea>
+                	<input type="submit" />
+                </form>
                 <Footer />
             </div>
         );
+    },
+
+    _handleSubmit: function(e) {
+    	e.preventDefault();
+    	var message = this.refs.message.getDOMNode().value.trim();
+    	alert(message);
+    	this.refs.message.getDOMNode().value = '';
+    	return;
     }
 });
 
